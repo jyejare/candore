3e0d65bac33c262ccffcb9a5b56bd32ace2bc49d
jyejare <jyejare@redhat.com>
2023-09-28 18:15:38 +0530

    Full Extraction - All Pages results

--- candore/candore.py
+++ candore/candore.py
@@ -16,36 +16,26 @@ def list_endpoints():
     return api_lister.lister_endpoints()
 
 
-async def save_all_entities(mode, api_lister=api_lister):
+async def save_all_entities(mode, output_file, full):
     """Save all the entities to a json file
 
     :param mode: Pre or Post
-    :param api_lister: The candore.modules.api_lister.APILister object
+    :param output_file: Output file name
+    :param full: If True, save entities from all pages of the components, else just saves first page
     :return: None
     """
     if mode not in ['pre', 'post']:
         raise ModeError("Extracting mode must be 'pre' or 'post'")
 
     async with Extractor(apilister=api_lister) as extractor:
+        if full:
+            extractor.full = True
         data = await extractor.extract_all_entities()
 
     if not data:
         click.echo('Entities data is not data found!')
 
-    file_path = Path(f'{mode}_entities.json')
-    with file_path.open(mode='w') as entfile:
-        json.dump(data, entfile)
-    click.echo(f'Entities data saved to {file_path}')
-    if mode not in ['pre', 'post']:
-        raise ModeError("Extracting mode must be 'pre' or 'post'")
-
-    async with Extractor(apilister=api_lister) as extractor:
-        data = await extractor.extract_all_entities()
-
-    if not data:
-        click.echo('Entities data is not data found!')
-
-    file_path = Path(f'{mode}_entities2.json')
+    file_path = Path(output_file) if output_file else Path(f'{mode}_entities.json')
     with file_path.open(mode='w') as entfile:
         json.dump(data, entfile)
     click.echo(f'Entities data saved to {file_path}')
